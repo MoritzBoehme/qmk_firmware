d032b7e43de413c6e58fc738ecf1ec35a3088ad4
Moritz Böhme <mail@moritzboeh.me>
2023-06-17 15:23:54 +0200

    feat: rename tap dance keycodes

--- users/MoritzBoehme/tap_dances.h
+++ users/MoritzBoehme/tap_dances.h
@@ -21,18 +21,22 @@ typedef struct {
 
 // Tap Dance enum
 enum {
-    BOOT,
-    SFT,
-    GUI,
+    _BOOT,
+    _LSFT,
+    _LGUI,
 };
+
+#define TD_BOOT TD(_BOOT)
+#define TD_LSFT TD(_LSFT)
+#define TD_LGUI TD(_LGUI)
 
 td_state_t cur_dance(tap_dance_state_t *state);
 
-void sft_finished(tap_dance_state_t *state, void *user_data);
-void sft_reset(tap_dance_state_t *state, void *user_data);
+void lsft_finished(tap_dance_state_t *state, void *user_data);
+void lsft_reset(tap_dance_state_t *state, void *user_data);
 
 void boot_finished(tap_dance_state_t *state, void *user_data);
 void boot_reset(tap_dance_state_t *state, void *user_data);
 
-void gui_finished(tap_dance_state_t *state, void *user_data);
-void gui_reset(tap_dance_state_t *state, void *user_data);
+void lgui_finished(tap_dance_state_t *state, void *user_data);
+void lgui_reset(tap_dance_state_t *state, void *user_data);
